edd485fffc97856fdd71c9430751c6a7502fe7bc
Daniel Guerrero <dansan_2002@hotmail.com>
2025-03-22 13:38:23 -0500

    Se repararon alguns CSS y se vio como utilizar los estados en React

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,7 +1,13 @@
 import "./CreateTodoButton.css";
 
 function CreateTodoButton() {
-	return <button className="CreateTodoButton">+</button>;
+	return (
+		<button
+			className="CreateTodoButton"
+			onClick={() => console.log("Le diste click")}>
+			+
+		</button>
+	);
 }
 
 export { CreateTodoButton };
